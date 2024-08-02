42e3aa9ee17d55e0d56423f66e0c8d844cf8999c
Ragul2703 <ragulkumar253@gmail.com>
2024-08-02 15:48:23 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -61,7 +61,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Login endpoint
-app.post('/login', async (req, res) => {
+app.post('/adminlogin.html', async (req, res) => {
   const { email, password } = req.body;
 
   try {
